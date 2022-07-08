80ecf3820dcc9224aaacb0d8084c48e527f4e3b0
oshknay5 <oshknay82@gmail.com>
2022-07-08 20:19:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const[tip, setTip]= useState('');
+
+  useEffect( ()=>{
+    getTips()
+  },[])
+
+  const  getTips = async() =>{
+  const response = await fetch('http://www.boredapi.com/api/activity' );
+  const data = await response.json();
+  setTip(data.activity)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className='container'>
+      <p className='tip'>{tip}</p>
+      <button className='btn' onClick={getTips}>New Tip</button>
     </div>
   );
 }
 
 export default App;
+
